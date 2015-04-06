ace714da4b2c0cc8ae86689da6ab9113be7406aa
chriszeng87 <chrisgardner87@gmail.com>
2015-04-06 22:39:24 +0800

    add Longest Substring Without Repeating Char solution 2

--- LongestSubstringWithoutRepeatingChar.java
+++ LongestSubstringWithoutRepeatingChar.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * Given a string, find the length of the longest substring without repeating characters. For example, 
  * the longest substring without repeating letters for "abcabcbb" is "abc", which the length is 3.
@@ -28,6 +30,24 @@ public class LongestSubstringWithoutRepeatingChar {
 		return retStr.toString();
 		
 	}
+	
+	static String getLongestSubstringWithoutRepeatingChar2(String str) {
+		if (str == null || str.length() == 0) {
+			return str;
+		}
+		HashMap<String, Integer> map = new HashMap<String, Integer>();
+		
+		StringBuilder retStr = new StringBuilder();
+		for (int i = 0; i < str.length(); i++) {
+			String sub = str.substring(i, i+1);
+			if(!map.containsKey(sub)) {
+				retStr.append(str.charAt(i));
+				map.put(sub, 1);
+			}
+		}
+		
+		return retStr.toString();
+	}
 
 	/**
 	 * @param args
@@ -35,9 +55,15 @@ public class LongestSubstringWithoutRepeatingChar {
 	public static void main(String[] args) {
 		String str = new String("abcabcbb");
 		System.out.println(getLongestSubstringWithoutRepeatingChar(str));
+		System.out.println(getLongestSubstringWithoutRepeatingChar2(str));
 		
 		String str2 = new String("bbbbb");
 		System.out.println(getLongestSubstringWithoutRepeatingChar(str2));
+		System.out.println(getLongestSubstringWithoutRepeatingChar2(str2));
+		
+		String str3 = new String("bbbbbc");
+		System.out.println(getLongestSubstringWithoutRepeatingChar(str3));
+		System.out.println(getLongestSubstringWithoutRepeatingChar2(str3));
 
 	}
 
