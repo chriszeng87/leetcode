a6da4d598736bbf3a646be23274f316be72b0dde
chriszeng87 <chrisgardner87@gmail.com>
2015-05-02 22:04:47 +0800

    update repeated DNA Sequence

--- RepeatedDNASequences.java
+++ RepeatedDNASequences.java
@@ -1,6 +1,8 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 /**
@@ -24,7 +26,7 @@ public class RepeatedDNASequences {
         
         Set<String> temp = new HashSet<String>();
     	Set<String> added = new HashSet<String>();
-        for (int i = 0; i < s.length() - 10; i++) {
+        for (int i = 0; i < s.length() - 9; i++) {
         	String str = s.substring(i, i + 10);
         	if (temp.contains(str) && !added.contains(str)) {
         		ret.add(str);
@@ -35,13 +37,34 @@ public class RepeatedDNASequences {
         }
         return ret;
     }
+    
+    public List<String> findRepeatedDnaSequences2(String s) {
+    	List<String> ret = new ArrayList<String>();
+        if (s == null || s.length() < 10) {
+        	return ret;
+        }
+        
+        Map<String, Integer> temp = new HashMap<String, Integer>();
+        for (int i = 0; i < s.length() - 9; i++) {
+        	String str = s.substring(i, i + 10);
+        	if (temp.containsKey(str) && temp.get(str) == 1) {
+        	    ret.add(str);
+        		temp.put(str, 2);
+        	} else if (!temp.containsKey(str)){
+        		temp.put(str, 1);
+        	}
+        }
+        return ret;
+
+
+    }
 	
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		String s = new String("AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT");
+		String s = new String("AAAAAAAAAAA");
 		System.out.println("return array = " + findRepeatedDnaSequences(s));
 
 	}
