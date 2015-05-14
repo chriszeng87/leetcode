3fbbc405610b582bd22ea005d3d4a08c6a47a803
chriszeng87 <chrisgardner87@gmail.com>
2015-05-14 08:30:52 +0800

    update TwoSum2 solution

--- TwoSum2.java
+++ TwoSum2.java
@@ -1,52 +1,63 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class TwoSum2 {
-	
-    public static int findIndex(int[] nums, int val) {
-        int left = 0, right = nums.length - 1;
-        while (left <= right) {
-            int mid = (left + right) / 2;
-            if (nums[mid] == val) {
-                return mid;
-            } else if (nums[mid] > val) {
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
-        }
-        return -1;
-    }
     
     public static int[] twoSum(int[] nums, int target) {
         int[] index = new int[2];
         if (nums == null || nums.length == 0) {
             return index;
         }
-        int[] numsclone = nums.clone();
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+        for (int i = 0; i < nums.length; i++) {
+        	map.put(nums[i], i + 1);
+        }
     
-        Arrays.sort(numsclone);
-        int i = 0, j = numsclone.length - 1;
+        Arrays.sort(nums);
+        int i = 0, j = nums.length - 1;
         int num1 = 0, num2 = 0;
         while (i < j) {
-            if (numsclone[i] + numsclone[j] == target) {
-                num1 = numsclone[i];
-                num2 = numsclone[j];
+            if (nums[i] + nums[j] == target) {
+                num1 = nums[i];
+                num2 = nums[j];
                 break;
-            } else if (numsclone[i] + numsclone[j] > target) {
+            } else if (nums[i] + nums[j] > target) {
                 j--;
             } else {
                 i++;
             }
         }
         
-        index[0] = findIndex(nums, num1) + 1;
-        index[1] = findIndex(nums, num2) + 1; 
+        index[0] = map.get(num1);
+        index[1] = map.get(num2); 
         
         if (index[0] > index[1]) {
             int tmp = index[0];
             index[0] = index[1];
             index[1] = tmp;
+        }
+        
+        return index;
+        
+    }
+    
+    public int[] twoSum2(int[] nums, int target) {
+        int[] index = new int[2];
+        if (nums == null || nums.length == 0) {
+            return index;
+        }
+        Map<Integer,Integer> map = new HashMap<Integer,Integer>();
+
+        for (int i = 0; i < nums.length; i ++) {
+        	if (map.containsKey(target - nums[i])) {
+        		index[0] = map.get(target - nums[i]) + 1;
+        		index[1] = i + 1;
+                return index;
+        	} else {
+        		map.put(nums[i],i);
+        	}
         }
         
         return index;
