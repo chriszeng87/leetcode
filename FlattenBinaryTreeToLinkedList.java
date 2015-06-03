ba486c9a53b76408746db67ab132f2cbbd376e46
chriszeng87 <chrisgardner87@gmail.com>
2015-06-03 09:11:28 +0800

    add Flatten Binary Tree To LinkedList solution

--- FlattenBinaryTreeToLinkedList.java
+++ FlattenBinaryTreeToLinkedList.java
@@ -1,51 +1,89 @@
-//import java.util.Stack;
-//
-///**
-// * 
-// * Definition for binary tree public class TreeNode { int val; TreeNode left;
-// * TreeNode right; TreeNode(int x) { val = x; } }
-// * 
-// * @author Chris
-// * 
-// */
-//
-//
-//
-//public class FlattenBinaryTreeToLinkedList {
-//
-//	public void flatten(TreeNode root) {
-//	        if(root == null) {
-//	            return;
-//	        }
-//	        
-//	        Stack<TreeNode> stack = new Stack<TreeNode>();
-//	        stack.push(root);
-//	        TreeNode pre = null;
-//	        while(!stack.isEmpty()) {
-//	            TreeNode p = stack.pop();
-//	            if(pre == null) {
-//	                pre = root;
-//	            } else {
-//	                pre.left = null;
-//	                pre.right = p;
-//	                pre = p;
-//	            }
-//	            if(p.right != null) {
-//	                stack.push(p.right);
-//	            }
-//	            if(p.left != null) {
-//	                stack.push(p.left);
-//	            }
-//	        }
-//	        
-//	    }
-//
-//	/**
-//	 * @param args
-//	 */
-//	public static void main(String[] args) {
-//		// TODO Auto-generated method stub
-//
-//	}
-//
-//}
+/**
+ * Given a binary tree, flatten it to a linked list in-place.
+
+For example,
+Given
+
+         1
+        / \
+       2   5
+      / \   \
+     3   4   6
+The flattened tree should look like:
+   1
+    \
+     2
+      \
+       3
+        \
+         4
+          \
+           5
+            \
+             6
+             
+ * @author Chris
+ *
+ */
+public class FlattenBinaryTreeToLinkedList {
+	
+    public static TreeNode getLeftMostRight(TreeNode root) {
+        if(root == null) {
+            return null;
+        }
+        
+        TreeNode leftMostRight  = root;
+        if(root.right != null) {
+        	leftMostRight = getLeftMostRight(root.right);
+        } else if (root.left != null){
+        	leftMostRight = getLeftMostRight(root.left);
+        }
+        
+        return leftMostRight;
+    }
+    
+    public static void flatten(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+        
+        TreeNode leftMostRight;
+        if (root.left == null) {
+        	leftMostRight = root;
+        } else {
+        	leftMostRight = getLeftMostRight(root.left);
+        }
+        
+        flatten(root.left);
+        flatten(root.right);
+
+        leftMostRight.right = root.right;
+        if (root.left != null) {
+        	root.right = root.left;
+        	root.left = null;
+        }
+        
+    }
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TreeNode[]  nodes = new TreeNode[6];
+		for (int i = 0; i < 6; i++) {
+			nodes[i] = new TreeNode(i + 1);
+		}
+		nodes[0].left = nodes[1];
+		nodes[0].right = nodes[4];
+		nodes[1].left = nodes[2];
+		nodes[1].right = nodes[3];
+		nodes[4].right = nodes[5];
+
+		flatten(nodes[0]);
+		Utils.prePrintTreeNode(nodes[0]);
+		System.out.println();
+		Utils.inPrintTreeNode(nodes[0]);
+	}
+
+}
