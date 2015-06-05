170060a5c41e1f5da945442f2073fa03b986c760
chriszeng87 <chrisgardner87@gmail.com>
2015-06-05 09:22:28 +0800

    add Triangle solution

--- Triangle.java
+++ Triangle.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /**
@@ -49,6 +50,45 @@ public class Triangle {
         minimumTotal(triangle, 0, 0, 0);
         return min;
     }
+    
+    public static int minimumTotal2(List<List<Integer>> triangle) {
+        if(triangle.size() == 0) {
+            return 0;
+        }
+        
+        int[] dp = new int[triangle.size()];
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        int[] dp2 = new int[triangle.size()];
+        Arrays.fill(dp2, Integer.MAX_VALUE);
+        dp[0] = triangle.get(0).get(0);
+        dp2[0] = triangle.get(0).get(0);
+        for (int i = 1; i < triangle.size(); i++) {
+            for (int j = 0; j <= i; j++) {
+            	if (j < i) {
+            		dp2[j] = dp[j] + triangle.get(i).get(j);
+            	}
+                if (j - 1 >= 0) {
+                    if (dp[j - 1] + triangle.get(i).get(j) < dp2[j]) {
+                        dp2[j] = dp[j - 1] + triangle.get(i).get(j);
+                    }
+                }
+                
+            }
+            
+            for (int k = 0; k <= i; k++) {
+                dp[k] = dp2[k];
+            }
+        }
+        
+        int min = dp[0];
+        for (int t = 1; t < triangle.size(); t++) {
+        	if (dp[t] < min) {
+        		min = dp[t]; 
+        	}
+        }
+        
+        return min;
+    }
 
 	/**
 	 * @param args
@@ -57,29 +97,29 @@ public class Triangle {
 		// TODO Auto-generated method stub
 		List<List<Integer>> triangle = new ArrayList<List<Integer>>();
 		List<Integer> list = new ArrayList<Integer>();
-		list.add(2);
+		list.add(-1);
 		
 		List<Integer> list2 = new ArrayList<Integer>();
 		list2.add(3);
-		list2.add(4);
+		list2.add(2);
 		
 		List<Integer> list3 = new ArrayList<Integer>();
-		list3.add(6);
-		list3.add(5);
-		list3.add(7);
+		list3.add(-3);
+		list3.add(1);
+		list3.add(-1);
 		
 		List<Integer> list4 = new ArrayList<Integer>();
 		list4.add(4);
 		list4.add(1);
 		list4.add(8);
-		list4.add(-5);
+		list4.add(3);
 		
 		triangle.add(list);
-		triangle.add(list2);
+		triangle.add(list2); 
 		triangle.add(list3);
-		triangle.add(list4);
+//		triangle.add(list4);
 		
-		int min = minimumTotal(triangle);
+		int min = minimumTotal2(triangle);
 		System.out.println("----min = " + min);
 		
 	}
