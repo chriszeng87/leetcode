22e46900fde40f2f744516518a0a00701c6080b5
chriszeng87 <chrisgardner87@gmail.com>
2015-06-13 13:22:28 +0800

    update Reverse Linked List solution

--- ReverseLinkedList.java
+++ ReverseLinkedList.java
@@ -27,13 +27,32 @@ public class ReverseLinkedList {
         
         return head;
     }
+    
+    public static ListNode reverseList2(ListNode head) {
+        if (head == null || head.next == null) {
+            return head;
+        }
+        
+        ListNode p = head.next;
+        ListNode reverseNext = reverseList2(p);
+        p.next = head;
+        head.next = null;
+        
+        return reverseNext;
+    }
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		ListNode node1 = new ListNode(1);
+		ListNode node2 = new ListNode(2);
+		ListNode node3 = new ListNode(3);
+		node1.next = node2;
+		node2.next = node3;
+		ListNode newHead = reverseList2(node1);
+		Utils.printNode(newHead);
 	}
 
 }
